026ab7364027cbe0d50afd5631174337d9f13ed7
ivand <ivan.d.kostov@gmail.com>
2023-07-28 15:56:01 +0300

    Decipher This

--- Python/SoftUni - Python Fundamentals/Lists_Advanced/Decipher This.py
+++ Python/SoftUni - Python Fundamentals/Lists_Advanced/Decipher This.py
@@ -1,19 +1,20 @@
 message = input().split()
 decrypted_msg = []
-number = ""
-second_part = ""
 
 for word in message:
+    number = ""
+    second_part = ""
+
     m = ord(word[2])
     if 48 <= m <= 57:
-        second_part = word[3::]
+        second_part = word[3:]
         number = (word[0] + word[1] + word[2])
         number = int(number)
     else:
-        second_part = word[2::]
+        second_part = word[2:]
         number = int(number + word[0] + word[1])
     if len(second_part) > 1:
-        temp = second_part[-1] + second_part[1:-1:] + second_part[0]
+        temp = second_part[-1] + second_part[1:-1] + second_part[0]
     else:
         temp = second_part
     decrypted_word = chr(number) + temp
