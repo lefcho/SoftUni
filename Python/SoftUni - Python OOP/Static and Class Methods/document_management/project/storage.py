bc8ef05db548401479cd259473c820019999d2b3
Ivan Kostov <ivan.d.kostov@gmail.com>
2023-11-18 22:17:12 +0200

    1 - 2

---
+++ Python/SoftUni - Python OOP/Static and Class Methods/document_management/project/storage.py
@@ -0,0 +1,24 @@
+from project.category import Category
+from project.topic import Topic
+from project.document import Document
+
+class Storage:
+    def __init__(self):
+        self.categories = []
+        self.topics = []
+        self.documents = []
+
+    def add_category(self, category: Category):
+        if category not in self.categories:
+            self.categories.append(category)
+
+    def add_topic(self, topic: Topic):
+        if topic not in self.topics:
+            self.topics.append(topic)
+
+    def add_document(self, document: Document):
+        if document not in self.documents:
+            self.documents.append(document)
+
+    def edit_category(self, category_id: int, new_name: str):
+        pass
