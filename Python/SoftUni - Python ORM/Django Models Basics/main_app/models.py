55462de34e450ec1fec48026bf9caa76d3dea8a0
ivand <ivan.d.kostov@gmail.com>
2024-06-20 21:33:01 +0300

    task 2

--- Python/SoftUni - Python ORM/Django Models Basics/main_app/models.py
+++ Python/SoftUni - Python ORM/Django Models Basics/main_app/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=30)
     age = models.PositiveIntegerField()
+
+
+class Blog(models.Model):
+    post = models.TextField()
+    author = models.CharField(max_length=35)
