8e445c529d461cbf8c6c274f87000bb9e40eecd0
ivand <ivan.d.kostov@gmail.com>
2024-06-20 23:36:38 +0300

    more solutions

--- Python/SoftUni - Python ORM/Django Models Basics/main_app/models.py
+++ Python/SoftUni - Python ORM/Django Models Basics/main_app/models.py
@@ -2,13 +2,17 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(
+        max_length=30,
+    )
     age = models.PositiveIntegerField()
 
 
 class Blog(models.Model):
     post = models.TextField()
-    author = models.CharField(max_length=35)
+    author = models.CharField(
+        max_length=35,
+    )
 
 
 class WeatherForecast(models.Model):
@@ -19,36 +23,77 @@ class WeatherForecast(models.Model):
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
     description = models.TextField()
     ingredients = models.TextField()
     cook_time = models.PositiveIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=70)
+    name = models.CharField(
+        max_length=70,
+    )
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
 
 
 class UserProfile(models.Model):
-    username = models.CharField(max_length=65, unique=True)
-    first_name = models.CharField(max_length=40, null=True)
-    last_name = models.CharField(max_length=40, null=True)
-    email = models.EmailField(unique=True, default="students@softuni.bg")
-    bio = models.TextField(max_length=120)
+    username = models.CharField(
+        max_length=65,
+        unique=True,
+    )
+    first_name = models.CharField(
+        max_length=40,
+        null=True,
+    )
+    last_name = models.CharField(
+        max_length=40,
+        null=True,
+    )
+    email = models.EmailField(
+        unique=True,
+        default="students@softuni.bg",
+    )
+    bio = models.TextField(
+        max_length=120,
+    )
     profile_image_url = models.URLField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
 
 
 class Exercise(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(
+        max_length=100,
+    )
     description = models.TextField()
-    difficulty_level = models.CharField(max_length=20)
+    difficulty_level = models.CharField(
+        max_length=20,
+    )
     duration_minutes = models.PositiveIntegerField()
-    equipment = models.CharField(max_length=90)
-    video_url = models.URLField(null=True, blank=True)
-    calories_burned = models.PositiveIntegerField(default=1)
-    is_favorite = models.BooleanField(default=False)
+    equipment = models.CharField(
+        max_length=90,
+    )
+    video_url = models.URLField(
+        null=True,
+        blank=True,
+    )
+    calories_burned = models.PositiveIntegerField(
+        default=1,
+    )
+    is_favorite = models.BooleanField(
+        default=False,
+    )
