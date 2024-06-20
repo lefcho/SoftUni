a8b0d4290d0d35f9242b766a9c38bec429376fec
ivand <ivan.d.kostov@gmail.com>
2024-06-20 23:20:46 +0300

    task 5

--- Python/SoftUni - Python ORM/Django Models Basics/main_app/models.py
+++ Python/SoftUni - Python ORM/Django Models Basics/main_app/models.py
@@ -31,3 +31,13 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=65, unique=True)
+    first_name = models.CharField(max_length=40, null=True)
+    last_name = models.CharField(max_length=40, null=True)
+    email = models.EmailField(unique=True, default="students@softuni.bg")
+    bio = models.TextField(max_length=120)
+    profile_image_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
