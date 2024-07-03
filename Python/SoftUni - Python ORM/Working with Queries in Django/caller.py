e84d4b4611177be414cddb7d6ca37c6c63e6bc94
ivand <ivan.d.kostov@gmail.com>
2024-07-03 23:46:13 +0300

    task 5 and 6

--- Python/SoftUni - Python ORM/Working with Queries in Django/caller.py
+++ Python/SoftUni - Python ORM/Working with Queries in Django/caller.py
@@ -10,7 +10,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 
-from main_app.models import ArtworkGallery, Laptop, LaptopOSChoices, ChessPlayer, Meal
+from main_app.models import ArtworkGallery, Laptop, LaptopOSChoices, ChessPlayer, Meal, Dungeon, Workout
 
 
 # Import your models
@@ -136,9 +136,80 @@ def delete_lunch_and_snack_meals():
     Meal.objects.filter(meal_type__in=('Lunch', 'Snack')).delete()
 
 
+def show_hard_dungeons():
+    hard_dungeons = Dungeon.objects.filter(difficulty='Hard').order_by('-location')
+
+    return '\n'.join(f'{d.name} is guarded by {d.boss_name} who has {d.boss_health} health points!' for d in hard_dungeons)
 
 
+def bulk_create_dungeons(args: List[Dungeon]):
+    Dungeon.objects.bulk_create(args)
 
+
+def update_dungeon_names():
+    Dungeon.objects.update(
+        name=Case(
+            When(difficulty='Easy', then=Value('The Erased Thombs')),
+            When(difficulty='Medium', then=Value('The Coral Labyrinth')),
+            When(difficulty='Hard', then=Value('The Lost Haunt'))
+        )
+    )
+
+
+def update_dungeon_bosses_health():
+    Dungeon.objects.exclude(difficulty='Easy').update(boss_health=500)
+
+
+def update_dungeon_recommended_levels():
+    Dungeon.objects.update(
+        recommended_level=Case(
+            When(difficulty='Easy', then=Value(25)),
+            When(difficulty='Medium', then=Value(50)),
+            When(difficulty='Hard', then=Value(75))
+        )
+    )
+
+
+def update_dungeon_rewards():
+    Dungeon.objects.filter(boss_health=500).update(reward='1000 Gold')
+    Dungeon.objects.filter(location__startswith='E').update(reward='New dungeon unlocked')
+    Dungeon.objects.filter(location__endswith='s').update(reward='Dragonheart Amulet')
+
+
+def set_new_locations():
+    Dungeon.objects.filter(recommended_level=25).update(location='Enchanted Maze')
+    Dungeon.objects.filter(recommended_level=50).update(location='Grimstone Mines')
+    Dungeon.objects.filter(recommended_level=75).update(location='Shadowed Abyss')
+
+
+def show_workouts():
+    cal_and_cf_workouts = Workout.objects.filter(workout_type__in=('Calisthenics', 'CrossFit')).order_by('id')
+
+    return '\n'.join(f'{w.name} from {w.workout_type} type has {w.difficulty} difficulty!' for w in cal_and_cf_workouts)
+
+
+def get_high_difficulty_cardio_workouts():
+    return Workout.objects.filter(workout_type='Cardio', difficulty='High').order_by('instructor')
+
+
+def set_new_instructors():
+    Workout.objects.filter(workout_type='Cardio').update(instructor='John Smith')
+    Workout.objects.filter(workout_type='Strength').update(instructor='Michael Williams')
+    Workout.objects.filter(workout_type='Yoga').update(instructor='Emily Johnson')
+    Workout.objects.filter(workout_type='CrossFit').update(instructor='Sarah Davis')
+    Workout.objects.filter(workout_type='Calisthenics').update(instructor='Chris Heria')
+
+
+def set_new_duration_times():
+    Workout.objects.filter(instructor='John Smith').update(duration='15 minutes')
+    Workout.objects.filter(instructor='Sarah Davis').update(duration='30 minutes')
+    Workout.objects.filter(instructor='Chris Heria').update(duration='45 minutes')
+    Workout.objects.filter(instructor='Michael Williams').update(duration='1 hour')
+    Workout.objects.filter(instructor='Emily Johnson').update(duration='1 hour and 30 minutes')
+
+
+def delete_workouts():
+    Workout.objects.exclude(workout_type__in=('Strength', 'Calisthenics')).delete()
 
 
 
