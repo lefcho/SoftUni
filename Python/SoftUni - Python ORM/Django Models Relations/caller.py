bbf9ce9a48dc311bf3486bb7bc9897583dba6503
ivand <ivan.d.kostov@gmail.com>
2024-07-05 17:35:49 +0300

    task 1

--- Python/SoftUni - Python ORM/Django Models Relations/caller.py
+++ Python/SoftUni - Python ORM/Django Models Relations/caller.py
@@ -6,5 +6,28 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Author, Book
 
-# Create queries within functions
+
+def show_all_authors_with_their_books():
+    authors_with_books = []
+
+    authors = Author.objects.all().order_by('id')
+
+    for author in authors:
+        books = author.book_set.all()
+
+        if not books:
+            continue
+
+        titles = ', '.join(b.title for b in books)
+        authors_with_books.append(
+            f'{author.name} has written - {titles}!'
+        )
+
+    return '\n'.join(authors_with_books)
+
+
+def delete_all_authors_without_books():
+    Author.objects.filter(book__isnull=True).delete()
+
