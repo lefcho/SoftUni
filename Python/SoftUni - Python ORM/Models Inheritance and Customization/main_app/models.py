6d27090bfc12fba95775cf8c9102e1a377eefd1b
ivand <ivan.d.kostov@gmail.com>
2024-07-09 19:46:04 +0300

    Task 3 and 4

--- Python/SoftUni - Python ORM/Models Inheritance and Customization/main_app/models.py
+++ Python/SoftUni - Python ORM/Models Inheritance and Customization/main_app/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -152,6 +153,55 @@ class Message(models.Model):
         message.save()
         return message
 
+
+class StudentIDField(models.PositiveIntegerField):
+    def to_python(self, value):
+        try:
+            return int(value)
+        except ValueError:
+            raise ValueError('Invalid input for student ID')
+
+    def get_prep_value(self, value):
+        value = self.to_python(value)
+
+        if value <= 0:
+            raise ValidationError('ID cannot be less than or equal to zero')
+
+        return value
+
+
+class Student(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    student_id = StudentIDField()
+
+
+class MaskedCreditCardField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 20
+        super().__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not isinstance(value, str):
+            raise ValidationError('The card number must be a string')
+
+        if not value.isdigit():
+            raise ValidationError('The card number must contain only digits')
+
+        if len(value) != 16:
+            raise ValidationError('The card number must be exactly 16 characters long')
+
+        return f'****-****-****-{value[-4:]}'
+
+
+class CreditCard(models.Model):
+    card_owner = models.CharField(
+        max_length=100,
+    )
+
+    card_number = MaskedCreditCardField()
 
 
 
