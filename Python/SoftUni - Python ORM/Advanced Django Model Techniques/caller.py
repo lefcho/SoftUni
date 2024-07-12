ef7c496c671410d3593b9771c382f28a71ea6548
ivand <ivan.d.kostov@gmail.com>
2024-07-12 20:57:04 +0300

    task 2 and 3

--- Python/SoftUni - Python ORM/Advanced Django Model Techniques/caller.py
+++ Python/SoftUni - Python ORM/Advanced Django Model Techniques/caller.py
@@ -1,7 +1,12 @@
 import os
+from decimal import Decimal
+
 import django
 from django.core.exceptions import ValidationError
 
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
+
+from main_app.models import Product, DiscountedProduct
