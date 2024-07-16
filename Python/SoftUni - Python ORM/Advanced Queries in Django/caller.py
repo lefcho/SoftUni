a521215a950a6ec73bd3478918be3731aed0aea2
ivand <ivan.d.kostov@gmail.com>
2024-07-16 20:34:36 +0300

    task 1

--- Python/SoftUni - Python ORM/Advanced Queries in Django/caller.py
+++ Python/SoftUni - Python ORM/Advanced Queries in Django/caller.py
@@ -5,6 +5,4 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
-
-# Run and print your queries
+from main_app.models import RealEstateListing
--- Python/SoftUni - Python ORM/Advanced Queries in Django/orm_skeleton/settings.py
+++ Python/SoftUni - Python ORM/Advanced Queries in Django/orm_skeleton/settings.py
@@ -75,23 +75,17 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "adv_queries",
+        "USER": "postgres",
+        "PASSWORD": "7896",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "",
-#         "USER": "",
-#         "PASSWORD": "",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
 
 
 
