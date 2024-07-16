78d5dad6d61e91958248710686c5ecb8888311f5
ivand <ivan.d.kostov@gmail.com>
2024-07-16 21:35:51 +0300

    task 5

--- Python/SoftUni - Python ORM/Advanced Queries in Django/caller.py
+++ Python/SoftUni - Python ORM/Advanced Queries in Django/caller.py
@@ -1,9 +1,10 @@
 import os
+from datetime import date
+
 import django
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice, Programmer, Project, Technology
-
+from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice, Programmer, Project, Technology, Task
--- Python/SoftUni - Python ORM/Advanced Queries in Django/main_app/models.py
+++ Python/SoftUni - Python ORM/Advanced Queries in Django/main_app/models.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django.db import models
+from django.db.models import F, Q
 
 from main_app.managers import RealEstateListingManager, VideoGameManager
 from main_app.validators import RangeValuesValidator
@@ -110,6 +113,34 @@ class Task(models.Model):
     is_completed = models.BooleanField(default=False)
     creation_date = models.DateField()
     completion_date = models.DateField()
+
+    @classmethod
+    def ongoing_high_priority_tasks(cls):
+        return cls.objects.filter(
+            priority='High',
+            is_completed=False,
+            completion_date__gt=F('creation_date'),
+        )
+
+    @classmethod
+    def completed_mid_priority_tasks(cls):
+        return cls.objects.filter(
+            priority='Medium',
+            is_completed=True,
+        )
+
+    @classmethod
+    def search_tasks(cls, query: str):
+        return cls.objects.filter(
+            Q(title__icontains=query) | Q(description__icontains=query),
+        )
+
+    @classmethod
+    def recent_completed_tasks(cls, days: int):
+        return cls.objects.filter(
+            is_completed=True,
+            completion_date__gte=F('creation_date') - timedelta(days=days)
+        )
 
 
 class Exercise(models.Model):
