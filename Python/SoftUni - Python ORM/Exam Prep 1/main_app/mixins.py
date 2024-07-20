4ae6a06630b6effc4e4f89643c3acd541830155a
ivand <ivan.d.kostov@gmail.com>
2024-07-20 21:53:42 +0300

    task 1 and 2

---
+++ Python/SoftUni - Python ORM/Exam Prep 1/main_app/mixins.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class AwardedMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    is_awarded = models.BooleanField(
+        default=False,
+    )
+
+
+class LastUpdatedMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    last_updated = models.DateTimeField(
+        auto_now=True,
+    )
--- Python/SoftUni - Python ORM/Exam Prep 1/main_app/models.py
+++ Python/SoftUni - Python ORM/Exam Prep 1/main_app/models.py
@@ -1,3 +1,129 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
+from main_app.mixins import AwardedMixin, LastUpdatedMixin
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    full_name = models.CharField(
+        max_length=120,
+        validators=[MinLengthValidator(2)],
+    )
+
+    birth_date = models.DateField(
+        default='1900-01-01',
+    )
+
+    nationality = models.CharField(
+        max_length=50,
+        default='Unknown',
+    )
+
+
+class Director(BaseModel):
+    years_of_experience = models.SmallIntegerField(
+        validators=[MinValueValidator(0)],
+        default=0,
+    )
+
+
+class Actor(BaseModel, AwardedMixin, LastUpdatedMixin):
+    pass
+
+
+class Movie(AwardedMixin, LastUpdatedMixin):
+    class GenreChoices(models.TextChoices):
+        ACTION = 'Action', 'Action'
+        COMEDY = 'Comedy', 'Comedy'
+        DRAMA = 'Drama', 'Drama'
+        OTHER = 'Other', 'Other'
+
+    title = models.CharField(
+        max_length=150,
+        validators=[MinLengthValidator(5)],
+    )
+
+    release_date = models.DateField()
+
+    storyline = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    genre = models.CharField(
+        max_length=10,
+        choices=GenreChoices.choices,
+        validators=[MinLengthValidator(6)],
+        default='Other',
+    )
+
+    rating = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=[MinValueValidator(0.0), MaxValueValidator(10.0)],
+        default=0.0,
+    )
+
+    is_classic = models.BooleanField(
+        default=False,
+    )
+
+    director = models.ForeignKey(
+        to=Director,
+        on_delete=models.CASCADE,
+        related_name='movies_directed',
+    )
+
+    starring_actor = models.ForeignKey(
+        to=Actor,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='starred_movies',
+    )
+
+    actors = models.ManyToManyField(
+        to=Actor,
+        related_name='movies',
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
