14e949c00fc5e92fc72588c6ce235a32497dfff0
ivand <ivan.d.kostov@gmail.com>
2024-07-21 01:31:10 +0300

    task 5

--- Python/SoftUni - Python ORM/Exam Prep 1/caller.py
+++ Python/SoftUni - Python ORM/Exam Prep 1/caller.py
@@ -42,11 +42,11 @@ def get_top_director():
 
 
 def get_top_actor():
-    top_actor = Actor.objects.prefetch_related('starred_movies')\
-                .annotate(movies_count=Count('starred_movies'),
-                          avg_ratind=Avg('starred_movies__rating'))\
-                .order_by('-movies_count', 'full_name')\
-                .first()
+    top_actor = Actor.objects.prefetch_related('starred_movies') \
+        .annotate(movies_count=Count('starred_movies'),
+                  avg_ratind=Avg('starred_movies__rating')) \
+        .order_by('-movies_count', 'full_name') \
+        .first()
 
     if not top_actor or not top_actor.movies_count:
         return ''
@@ -55,6 +55,57 @@ def get_top_actor():
 
     return (f"Top Actor: {top_actor.full_name}, starring in movies: {movies},"
             f" movies average rating: {top_actor.avg_ratind:.1f}")
+
+
+def get_actors_by_movies_count():
+    actors = Actor.objects.prefetch_related('movies') \
+                 .annotate(movies_count=Count('movies')) \
+                 .order_by('-movies_count', 'full_name')[:3]
+
+    if not actors or not actors[0].movies_count:
+        return ''
+
+    return '\n'.join(f'{a.full_name}, participated in {a.movies_count} movies' for a in actors)
+
+
+def get_top_rated_awarded_movie():
+    top_movie = (Movie.objects.select_related('starring_actor')
+                 .prefetch_related('actors')
+                 .filter(is_awarded=True)
+                 .order_by('-rating', 'title')
+                 .first())
+
+    if not top_movie:
+        return ''
+
+    starring_actor = top_movie.starring_actor.full_name if top_movie.starring_actor else 'N/A'
+
+    participating_actors = top_movie.actors.order_by('full_name').values_list('full_name', flat=True)
+
+    cast = ', '.join(participating_actors)
+
+    return (f"Top rated awarded movie: {top_movie.title}, rating: {top_movie.rating}."
+            f" Starring actor: {starring_actor}. Cast: {cast}.")
+
+
+def increase_rating():
+    movies_to_update = Movie.objects.filter(is_classic=True, rating__lte=9.9)
+
+    if not movies_to_update:
+        return 'No ratings increased.'
+
+    updated_movies_count = movies_to_update.count()
+    movies_to_update.update(rating=F('rating') + 0.1)
+    return f'Rating increased for {updated_movies_count} movies.'
+
+
+
+
+
+
+
+
+
 
 
 
