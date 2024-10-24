129de39cddb321de31cb49096941b69b13d9e98b
ivand <ivan.d.kostov@gmail.com>
2024-10-25 00:58:19 +0300

    added: albums and profiles app + models

---
+++ Python/SoftUni - Django Basics/Exam Prep 1/examPrep1/profiles/validators.py
@@ -0,0 +1,25 @@
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+from django.utils.text import slugify
+
+
+@deconstructible
+class AlphaNumericalValidator:
+    def __init__(self, message=None):
+        self.message = message
+
+    @property
+    def message(self):
+        return self.__message
+
+    @message.setter
+    def message(self, value):
+        if value is None:
+            self.__message = 'Must contain only numbers, letters and underscore'
+        else:
+            self.__message = value
+
+    def __call__(self, value, *args, **kwargs):
+        slugged_value = slugify(value)
+        if '-' in value or value != slugged_value:
+            raise ValidationError(self.message)
