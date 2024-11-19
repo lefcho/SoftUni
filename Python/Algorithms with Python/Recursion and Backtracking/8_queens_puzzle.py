bcb6ed2a1c13605b7b5844cfc201e661c78fceba
ivand <ivan.d.kostov@gmail.com>
2024-11-19 23:33:46 +0200

    stats 8_queens_puzzle.py

---
+++ Python/Algorithms with Python/Recursion and Backtracking/8_queens_puzzle.py
@@ -0,0 +1,34 @@
+def print_board(board):
+    for row in board:
+        print(' '.join(row))
+    print()
+
+
+def can_place_queen(row, col):
+    return 1
+
+
+def set_queen(row, col, board):
+    pass
+
+
+def remove_queen(row, col, board):
+    pass
+
+
+def put_queens(row, board, rows, cols, diagonal_1, diagonal_2):
+    if row == 8:
+        print_board(board)
+        return
+    for col in range(8):
+        if can_place_queen(row, col):
+            set_queen(row, col, board)
+            put_queens(row + 1, board, rows, cols, diagonal_1, diagonal_2)
+            remove_queen(row, col, board)
+
+
+n = 8
+board = []
+[board.append(['-'] * n) for _ in range(n)]
+
+put_queens(0, board, set(), set(), set(), set())
